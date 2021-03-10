fc857a215d51038e6096a43c7eca7de8b7c88c94
Andrew George <aggeorge26@gmail.com>
2021-03-10 13:11:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+'''
+For in Loop
+'''
+#students = ['Bob', 'Adriana', 'Felix']
+
+#for x in students:
+ #   print("hi", x)
+
+# ==========================
+
+#print(range(10))
+#print(list(range(10)))
+#print(list(range(3, 6)))
+#print(list(range(3, 12, 3)))
+
+# ==========================
+
+#for x in range(1,6,2): 
+ # print( x * 2 )
+
+#for i in range(1,30):
+ # for j in range(i):
+  #  print("L", end="")
+   # print()
+
+
+# ==========================
+
+'''
+While in Loop
+'''
+#i = 1
+#while i < 10:
+ # print(i)
+  #i += 1
+
+# ==========================
+#break statements
+
+#i = 1
+#while i < 10:
+ # print(i)
+  #if i == 7:
+   #break
+  #i += 1 
+
+# ==========================
+# continue statements
+
+#i = 1
+#while i < 5:
+#    i += 1
+#    if i == 4:
+#        continue
+#    print(i)
+
+# ==========================
+
+#for i in range(1, 10):
+#    if i == 3:
+#      break
+#    print(i)
+
+
